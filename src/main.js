8f81cb79374832d584c475f29e980a4676e4c489
Kirat Arora <kiratarora@umass.edu>
2023-04-02 20:43:32 -0400

    Basic card structure placed

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
 window.onload = () => {
     const cardsDiv = document.getElementById("cardsListDiv");
-    for (let i = 0; i < 10; i++) {
-        fetch('components/movieCard.html')
-            .then(response => response.text())
-            .then(html => {
-                const cardComponent = document.createElement("div");
-                cardComponent.innerHTML = html;
-                cardsDiv.appendChild(cardComponent);
-            })
-            .catch(error => console.error(error));
-    }
+    fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            fetch('components/movieCard.html')
+                .then(response => response.text())
+                .then(html => {
+                    html = html.replace("Image Address", data[i].src);
+                    html = html.replace("Movie Name", data[i].name);
+                    html = html.replace("Comment", data[i].comment_titile);
+                    let stars = '';
+                    for (let j = 1; j<=5; ++j){
+                        if(j<=data[i].stars){
+                            stars += "&#9733; ";
+                        }
+                        else{
+                            stars += "&#9734; ";
+                        }
+                    }
+                    stars = stars.slice(0, -1);
+                    html = html.replace("Star Rating", stars);
+                    const cardComponent = document.createElement("div");
+                    cardComponent.innerHTML = html;
+                    cardsDiv.appendChild(cardComponent);
+                })
+                .catch(error => console.error(error));
+        }
+    })
   };
   
