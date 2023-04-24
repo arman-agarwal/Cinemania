91e7d2bb286ae7fd56b5d8294fe2986abfc8506a
Arman Agarwal <armanagarwal0308@gmail.com>
2023-04-24 04:30:15 -0400

    added title bar to login page, added button for signing up, and some design changes

--- src/login.js
+++ src/login.js
@@ -1,7 +1,30 @@
-let formBox = document.getElementById("loginPage");
-let x = document.createElement("div");
-fetch("components/loginForm.html")
-  .then((response) => response.text())
-  .then((html) => (x.innerHTML = html));
-  console.log(formBox);
-formBox.appendChild(x);
+window.onload = () => {
+  let spanTexts = document.getElementsByClassName("backLetter");
+  for (spanText of spanTexts) {
+    spanText.classList.add("active");
+  }
+  let formBody = document.createElement("div");
+
+  fetch("components/loginForm.html")
+    .then((response) => response.text())
+    .then((html) => (formBody.innerHTML = html.trim()));
+  document.getElementById("loginPage").appendChild(formBody);
+};
+
+let spanLetters = document.getElementsByClassName("backLetter");
+let spanTexts = document.getElementById("backText");
+
+spanTexts.addEventListener("mouseover", function () {
+  for (spanLetter of spanLetters) {
+    spanLetter.classList.remove("active");
+    spanLetter.classList.add("activeG");
+  }
+});
+
+// Add a mouseout event listener to the div element
+spanTexts.addEventListener("mouseout", function () {
+  for (spanLetter of spanLetters) {
+    spanLetter.classList.remove("activeG");
+    spanLetter.classList.add("active");
+  }
+});
