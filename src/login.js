23ec73988aea8a2c8b2d2b4c42ec23129105fd7a
Arman Agarwal <armanagarwal0308@gmail.com>
2023-04-24 00:55:10 -0400

    added basic framework for login

---
+++ src/login.js
@@ -0,0 +1,7 @@
+let formBox = document.getElementById("loginBody");
+let x = document.createElement("div");
+fetch("components/loginForm.html")
+  .then((response) => response.text())
+  .then((html) => (x.innerHTML = html));
+  console.log(formBox);
+formBox.appendChild(x);
