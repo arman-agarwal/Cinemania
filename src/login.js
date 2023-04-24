4d6f6b37f4d73702c3e18bd53eb60f787668582f
Arman Agarwal <armanagarwal0308@gmail.com>
2023-04-24 16:00:01 -0400

    worked on signup page, added fields, formatting, username suggestions

--- src/login.js
+++ src/login.js
@@ -1,22 +1,22 @@
 let loginPage = document.getElementById("loginPage");
 let loginBody = document.createElement("div");
-loginBody.setAttribute("id","replace");
+loginBody.setAttribute("id", "replace");
 let signupBody = document.createElement("div");
-signupBody.setAttribute("id" ,"replace");
+signupBody.setAttribute("id", "replace");
 window.onload = () => {
   let spanTexts = document.getElementsByClassName("backLetter");
   for (spanText of spanTexts) {
     spanText.classList.add("active");
   }
-  
+
   fetch("components/loginForm.html")
     .then((response) => response.text())
     .then((html) => (loginBody.innerHTML = html.trim()));
-    fetch("components/signUp.html")
+  fetch("components/signUp.html")
     .then((response) => response.text())
     .then((html) => (signupBody.innerHTML = html.trim()));
   loginPage.appendChild(loginBody);
-}
+};
 let spanLetters = document.getElementsByClassName("backLetter");
 let spanTexts = document.getElementById("backText");
 
@@ -34,24 +34,60 @@ spanTexts.addEventListener("mouseout", function () {
     spanLetter.classList.add("active");
   }
 });
-let signupButton,loginButton;
-function refreshButtons(){
+let signupButton, loginButton;
+function refreshButtons() {
   signupButton = document.getElementById("signupButton");
   loginButton = document.getElementById("loginButton");
-  if(signupButton!=undefined){
-    signupButton.addEventListener("click",()=>{
+  if (signupButton != undefined) {
+    signupButton.addEventListener("click", () => {
       loginPage.removeChild(document.getElementById("replace"));
       loginPage.appendChild(signupBody);
       refreshButtons();
+      setTimeout(() => {
+        refreshFields();
+      }, 50);
     });
-  };
-  if(loginButton!=undefined){
-    loginButton.addEventListener("click",()=>{
+  }
+  if (loginButton != undefined) {
+    loginButton.addEventListener("click", () => {
       loginPage.removeChild(document.getElementById("replace"));
       loginPage.appendChild(loginBody);
       refreshButtons();
     });
-  };
+  }
+}
+
+function refreshFields() {
+  let LastName = document.getElementById("lastName");
+  let firstName = document.getElementById("firstName");
+  let emailFormat = "@gmail.com";
+  if (LastName != null) {
+    LastName.addEventListener("keyup", () => {
+      if (firstName.value == "" || LastName.value == "") setDefault();
+      else {
+        document.getElementById("username").placeholder =
+          firstName.value + LastName.value;
+        document.getElementById("email").placeholder =
+          firstName.value + LastName.value + emailFormat;
+      }
+    });
+  }
+  if (firstName != null) {
+    firstName.addEventListener("keyup", () => {
+            if (firstName.value == "" || LastName.value == "") setDefault();
+      else{
+      document.getElementById("username").placeholder =
+        firstName.value + LastName.value;
+      document.getElementById("email").placeholder =
+        firstName.value + LastName.value + "@gmail.com";
+      }
+    });
+  }
+}
+
+function setDefault() {
+  document.getElementById("username").placeholder = "username";
+  document.getElementById("email").placeholder = "email@gmail.com";
 }
 
 setTimeout(() => {
