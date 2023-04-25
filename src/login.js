084dde921ec55bbba5ca0acec7c514800bf1f238
Arman Agarwal <armanagarwal0308@gmail.com>
2023-04-24 20:22:38 -0400

    refactored a lot of code, and cleaned up, also resolved error that cause multiple alerts to show up instead of 1

--- src/login.js
+++ src/login.js
@@ -4,19 +4,50 @@ let signupButton, loginButton, submitButton;
 loginBody.setAttribute("id", "replace");
 let signupBody = document.createElement("div");
 signupBody.setAttribute("id", "replace");
-window.onload = () => {
+let x = false;
+window.onload = async () => {
   let spanTexts = document.getElementsByClassName("backLetter");
   for (spanText of spanTexts) {
     spanText.classList.add("active");
   }
 
-  fetch("components/loginForm.html")
+  await fetch("components/loginForm.html")
     .then((response) => response.text())
     .then((html) => (loginBody.innerHTML = html.trim()));
-  fetch("components/signUp.html")
+  await fetch("components/signUp.html")
     .then((response) => response.text())
     .then((html) => (signupBody.innerHTML = html.trim()));
-  loginPage.children[1].insertAdjacentElement("beforebegin",loginBody);
+
+  loginPage.children[1].insertAdjacentElement("beforebegin", loginBody);
+
+  let loginButtonList = loginBody.children[0].children[0];
+  let signupButtonList = signupBody.children[0].children[0];
+
+  loginButtonList.children[4].addEventListener("click", () => {
+    loginPage.removeChild(document.getElementById("replace"));
+    loginPage.children[1].insertAdjacentElement("beforebegin", signupBody);
+    if (!x) refreshFields();
+    x = true;
+  });
+
+  signupButtonList.children[7].addEventListener("click", () => {
+    loginPage.removeChild(document.getElementById("replace"));
+    loginPage.children[1].insertAdjacentElement("beforebegin", loginBody);
+  });
+
+  loginButtonList.children[3].addEventListener("click", () => {
+    location.href = "index.html";
+  });
+  console.log(signupButtonList.children[4].children[1]);
+  signupButtonList.children[6].addEventListener("click", () => {
+    if (
+      checkData(
+        signupButtonList.children[4].children[1],
+        signupButtonList.children[5].children[1].value
+      )
+    )
+      location.href = "index.html";
+  });
 };
 let spanLetters = document.getElementsByClassName("backLetter");
 let spanTexts = document.getElementById("backText");
@@ -36,60 +67,28 @@ spanTexts.addEventListener("mouseout", function () {
   }
 });
 
-function refreshButtons() {
-  signupButton = document.getElementById("signupButton");
-  loginButton = document.getElementById("loginButton");
-  submitButton = document.getElementById("submit");
-  if(submitButton!=undefined){
-  submitButton.addEventListener("click", () => {
-    if (checkData() != false) {
-      location.href = "index.html";
-    }
-  });
-  }
-
-  if (signupButton != undefined) {
-    signupButton.addEventListener("click", () => {
-      loginPage.removeChild(document.getElementById("replace"));
-      loginPage.children[1].insertAdjacentElement("beforebegin",signupBody);
-      refreshButtons();
-      setTimeout(() => {
-        refreshFields();
-      }, 10);
-    });
-  }
-
-  if (loginButton != undefined) {
-    loginButton.addEventListener("click", () => {
-      loginPage.removeChild(document.getElementById("replace"));
-      loginPage.children[1].insertAdjacentElement("beforebegin",loginBody);
-      refreshButtons();
-    });
-  }
-}
-
 function refreshFields() {
-  let LastName = document.getElementById("lastName");
+  let lastName = document.getElementById("lastName");
   let firstName = document.getElementById("firstName");
-  if (LastName != null) {
-    LastName.addEventListener("keyup", () => {
-      if (firstName.value == "" || LastName.value == "") setDefault();
+  if (lastName != null) {
+    lastName.addEventListener("keyup", () => {
+      if (firstName.value == "" || lastName.value == "") setDefault();
       else {
         document.getElementById("username").placeholder =
-          firstName.value + LastName.value;
+          firstName.value + lastName.value;
         document.getElementById("email").placeholder =
-          firstName.value + LastName.value + "@gmail.com";
+          firstName.value + lastName.value + "@gmail.com";
       }
     });
   }
   if (firstName != null) {
     firstName.addEventListener("keyup", () => {
-      if (firstName.value == "" || LastName.value == "") setDefault();
+      if (firstName.value == "" || lastName.value == "") setDefault();
       else {
         document.getElementById("username").placeholder =
-          firstName.value + LastName.value;
+          firstName.value + lastName.value;
         document.getElementById("email").placeholder =
-          firstName.value + LastName.value + "@gmail.com";
+          firstName.value + lastName.value + "@gmail.com";
       }
     });
   }
@@ -107,68 +106,53 @@ function checkStrength(password) {
   if (
     "string" !== typeof password ||
     password.length < 5 ||
-    (firstName !== undefined && firstName.value.length!==0 && password.includes(firstName.value)) ||
-    (lastName !== undefined && lastName.value.length!==0 && password.includes(lastName.value)) ||
-    (username !== undefined && username.value.length!==0 &&  password.includes(username.value))
+    (firstName !== undefined &&
+      firstName.value.length !== 0 &&
+      password.includes(firstName.value)) ||
+    (lastName !== undefined &&
+      lastName.value.length !== 0 &&
+      password.includes(lastName.value)) ||
+    (username !== undefined &&
+      username.value.length !== 0 &&
+      password.includes(username.value))
   ) {
     return false;
   }
   return true;
 }
 
-function checkData() {
-  let email = document.getElementById("email");
-  if (email != null) {
-    let regex = /\S+@\S+\.\S+$/;
-    if (!regex.test(email.value)) {
-      alert("invalid email");
-      return false;
-    }
+function checkData(email, password) {
+  let regex = /\S+@\S+\.\S+$/;
+  if (!regex.test(email.value)) {
+    alert("invalid email");
+    return false;
   }
-  let password = document.getElementById("password").value;
-  if (document.getElementById("login") === null) {
-    if (!checkStrength(password)) {
-      alert("weak password");
-      return false;
-    }
+  if (!checkStrength(password)) {
+    alert("weak password");
+    return false;
   }
   return true;
 }
 
-setTimeout(() => {
-  refreshButtons();
-}, 50);
-
-
 //dark Mode code
 const light = document.getElementById("lightButton");
-  const dark = document.getElementById("darkButton");
-  const sun = document.getElementById("sunImg");
-  const moon = document.getElementById("moonImg");
-  const body = document.body;
-  const profileButton = document.getElementById("profileButton");
+const dark = document.getElementById("darkButton");
+const sun = document.getElementById("sunImg");
+const moon = document.getElementById("moonImg");
+const body = document.body;
 
+light.addEventListener("click", function () {
+  moon.classList.add("img-faded");
+  moon.classList.remove("img-normal");
+  sun.classList.add("img-normal");
+  sun.classList.remove("img-faded");
+  body.classList.remove("dark-mode");
+});
 
-
-  light.addEventListener("click", function() {
-    moon.classList.add("img-faded");
-    moon.classList.remove("img-normal");
-    sun.classList.add("img-normal");
-    sun.classList.remove("img-faded");
-    body.classList.remove("dark-mode");
-    profileButton.classList.add("btn-outline-success");
-    profileButton.classList.remove("btn-danger");
-
-  });
-  
-  dark.addEventListener("click", function() {
-    moon.classList.remove("img-faded");
-    moon.classList.add("img-normal");
-    sun.classList.remove("img-normal");
-    sun.classList.add("img-faded");
-    body.classList.add("dark-mode");
-    profileButton.classList.remove("btn-outline-success");
-    profileButton.classList.add("btn-danger");
-
-  });
-
+dark.addEventListener("click", function () {
+  moon.classList.remove("img-faded");
+  moon.classList.add("img-normal");
+  sun.classList.remove("img-normal");
+  sun.classList.add("img-faded");
+  body.classList.add("dark-mode");
+});
